f30e61e59a455d8920d1512f6a984951e39dbdc2
Jose Ramos <jmrp.04@gmail.com>
2019-02-16 23:24:57 -0500

    First commit: Creating the project PitchPerfect

---
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Jose Ramos on 2/16/19.
+//  Copyright © 2019 Jose Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
