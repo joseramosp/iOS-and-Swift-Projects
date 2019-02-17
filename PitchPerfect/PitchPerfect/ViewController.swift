b5c5b87572d64aeb33c288739dc2b3021235b7ce
Jose Ramos <jmrp.04@gmail.com>
2019-02-17 17:07:52 -0500

    Added a new viewController and view controller navigation

--- PitchPerfect/PitchPerfect/ViewController.swift
+++ PitchPerfect/PitchPerfect/ViewController.swift
@@ -11,19 +11,32 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        stopRecordingButton.isEnabled = false
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillApper called")
     }
 
     @IBAction func RecordAudio(_ sender: Any) {
-        print("Record button was pressed")
-        recordLabel.text = "Recording..."
+        //print("Record button was pressed")
+        recordLabel.text = "Recording in progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
     
     @IBAction func stopRecording(_ sender: Any) {
-        print("Stop recording button was pressed")
+        //print("Stop recording button was pressed")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordLabel.text = "Tap to record"
     }
 }
 
